444f6389259a8f3a0db1987080bf0affb36f4140
Franko Stipanov <fstipanov@gmail.com>
2023-06-29 07:33:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Advice</button>
+      <p>
+        You get the advice <strong>{count}</strong> times.
+      </p>
+    </div>
+  );
+}
